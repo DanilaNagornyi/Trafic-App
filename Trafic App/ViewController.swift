05c2422cce5e9ecbdc9a9e3b181bfdcf38e5da5f
Danila Nagornyi <excelrin@gmail.com>
2020-04-22 16:43:35 +0300

    Did icon App

--- Trafic App/ViewController.swift
+++ Trafic App/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    enum CurrentLight {
+        case red, yellow, green
+    }
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var nextColorButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    
+    private let light: CGFloat = 1
+    private let dark: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nextColorButton.layer.cornerRadius = 15
+        nextColorButton.setTitle("Go!", for: .normal)
+        
+        redLight.alpha = dark
+        yellowLight.alpha = dark
+        greenLight.alpha = dark
     }
+    
+    override func viewDidLayoutSubviews() {
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+    }
+    
 
-
+    @IBAction func clickNextColor() {
+        
+        nextColorButton.setTitle("Next Light", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = dark
+            redLight.alpha = light
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = dark
+            yellowLight.alpha = light
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = dark
+            greenLight.alpha = light
+            currentLight = .red
+        }
+    }
+    
 }
 
